d3ea652a1a96ab1d97fc67e27d90ba1171bea3eb
Fink Patrick <finpac14@htlkaindorf.at>
2018-09-26 12:49:03 +0200

    Edited

--- SerializeIntro/src/intro/Schueler.java
+++ SerializeIntro/src/intro/Schueler.java
@@ -5,10 +5,32 @@
  */
 package intro;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Patrick
  */
 public class Schueler {
+    private String name;
+    private int age;
+    private LocalDate birthday;
+
+    public Schueler(String name, int age, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+        this.age = LocalDate.now().getYear() - birthday.getYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    
+    
+    
     
 }
